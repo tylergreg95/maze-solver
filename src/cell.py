8d8ab904f9adf924f1e713bf57d0badc62cc43eb
Tyler Gregory <tylergreg95@gmail.com>
2024-06-25 17:23:29 -0400

    added method to draw path between two cells

--- src/cell.py
+++ src/cell.py
@@ -29,3 +29,10 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        start_center = Point((self._x1 + self._x2)/2, (self._y1 + self._y2)/2)
+        to_cell_center = Point((to_cell._x1 + to_cell._x2)/2, (to_cell._y1 + to_cell._y2)/2)
+        path = Line(start_center, to_cell_center)
+        color = "gray" if undo else "red"
+        self._win.draw_line(path, color)
--- src/main.py
+++ src/main.py
@@ -4,21 +4,13 @@ from cell import Cell
 def main():
     win = Window(800, 600)
     
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.draw(50, 50, 100, 100)
 
-    c=Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.draw(150, 50, 200, 100)
 
-    c=Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
-
-    c=Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c1.draw_move(c2)
 
     win.wait_for_close()
 
